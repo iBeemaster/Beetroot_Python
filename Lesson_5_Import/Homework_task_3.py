d955b37d96d2d7464f58c6e3553c5c3706802893
iBeemaster <pavel.lankin@gmail.com>
2022-02-14 15:35:49 +0200

    Words combination

---
+++ Lesson_5_Import/Homework_task_3.py
@@ -0,0 +1,12 @@
+# Words combination
+# Create a program that reads an input string and then creates and prints 5 random strings from characters of the input string.
+# For example, the program obtained the word ‘hello’, so it should print 5 random strings(words)
+# that combine characters ‘h’, ‘e’, ‘l’, ‘l’, ‘o’ -> ‘hlelo’, ‘olelh’, ‘loleh’ …
+# Tips: Use random module to get random char from string)
+
+from random import shuffle
+s = list(input('Введите текст для обработки: '))
+for i in range(5):
+    shuffle(s)
+    n = ''.join(s)
+    print(n)
